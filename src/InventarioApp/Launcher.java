98f601c554ee9d7a2477ddb06b3b9f2c8bc52cac
CarePerro0926 <programacion0926@gmail.com>
2026-09-08 14:05:52 -0500

    correcion para ejecutarlo en antigravity

---
+++ src/InventarioApp/Launcher.java
@@ -0,0 +1,10 @@
+package InventarioApp;
+
+/**
+ * Clase lanzadora para inicializar JavaFX sin problemas de módulos o classloader.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
